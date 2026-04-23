c22475344042e790ab5934148f75dfa03b017a17
ShadowOfDeath25 <abdo.251152@gmail.com>
2026-04-23 23:49:29 +0200

    Refactor broadcasting channels; improve code formatting and remove unused imports

--- app/Events/SummarizationFinished.php
+++ app/Events/SummarizationFinished.php
@@ -5,7 +5,6 @@ namespace App\Events;
 use AllowDynamicProperties;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
@@ -32,7 +31,7 @@ class SummarizationFinished implements ShouldBroadcast
     public function broadcastOn(): array
     {
         return [
-            new PrivateChannel('notes.' . $this->noteId),
+            new PrivateChannel('notes.'.$this->noteId),
         ];
     }
 }
