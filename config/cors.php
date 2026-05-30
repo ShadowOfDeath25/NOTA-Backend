faca4b7e55bb74a70c0afad56ae9cc3313f35c20
ShadowOfDeath25 <abdo.251152@gmail.com>
2026-05-30 22:55:52 +0300

    Hopefully this fixes the CSRF bug

--- config/cors.php
+++ config/cors.php
@@ -22,8 +22,9 @@ return [
     //    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', '*')),
     'allowed_origins' => ['*'],
 
-    'allowed_origins_patterns' => [],
-
+    'allowed_origins_patterns' => [
+        '/^https:\/\/.*\.ngrok-free\.dev$/',
+    ],
     'allowed_headers' => ['*'],
 
     'exposed_headers' => [],
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -32,7 +31,7 @@ return new class extends Migration
 
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
-            $table->foreignIdFor(User::class);
+            $table->foreignUuid('user_id')->nullable()->constrained()->cascadeOnDelete();
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
             $table->longText('payload');
