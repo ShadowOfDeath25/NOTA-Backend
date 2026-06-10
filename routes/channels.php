fa5076e665ec971cdb1572d2cfd3079aa26aaf66
ShadowOfDeath25 <abdo.251152@gmail.com>
2026-06-10 23:47:49 +0300

    Addded logging for websockets auth

--- routes/channels.php
+++ routes/channels.php
@@ -4,7 +4,12 @@ use App\Models\Note;
 use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return  $user->id ===  $id;
+    logger()->info('Broadcast auth', [
+        'user' => $user?->id,
+        'channel_id' => $id,
+    ]);
+
+    return $user->id === $id;
 }, ['middleware' => ['auth:sanctum']]);
 
 Broadcast::channel('note.{note}', function ($user, Note $note): bool {
