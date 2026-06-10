10d0eb4ac2ca09bc2892043baae435f6386ecba7
ShadowOfDeath25 <abdo.251152@gmail.com>
2026-06-10 23:39:49 +0300

    removed type casting in websockets auth check

--- routes/channels.php
+++ routes/channels.php
@@ -4,7 +4,7 @@ use App\Models\Note;
 use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+    return  $user->id ===  $id;
 }, ['middleware' => ['auth:sanctum']]);
 
 Broadcast::channel('note.{note}', function ($user, Note $note): bool {
