3664253f00bf7af7be6fa6070c67aa6a5c7c498e
ShadowOfDeath25 <abdo.251152@gmail.com>
2026-06-11 19:43:21 +0300

    Define table in FavoriteNote pivot and add is_favorite attribute to Note model

--- app/Models/FavoriteNote.php
+++ app/Models/FavoriteNote.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Relations\Pivot;
 
 class FavoriteNote extends Pivot
 {
+    protected $table = "favorite_notes";
     protected $fillable = [
         'user_id',
         'note_id',
